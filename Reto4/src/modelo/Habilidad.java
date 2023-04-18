059120ad3fcebb77e92c62b2705e4db5128a9942
in1dm3 <in1dm3@HZ352013.elorrieta.int>
2023-04-18 13:07:47 +0200

    Habilidad y personaje

--- Reto4/src/modelo/Habilidad.java
+++ Reto4/src/modelo/Habilidad.java
@@ -1,5 +1,45 @@
-package modelo;
 
 public class Habilidad {
-
+	
+	//Valores
+	
+	private String nombre;
+	private Personaje personaje;
+	private String descripcion;
+	
+	//getter setter
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Personaje getPersonaje() {
+		return personaje;
+	}
+	public void setPersonaje(Personaje personaje) {
+		this.personaje = personaje;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	//constructor
+	
+	public Habilidad(String nombre, Personaje personaje, String descripcion) {
+		super();
+		this.nombre = nombre;
+		this.personaje = personaje;
+		this.descripcion = descripcion;
+	}
+	
+	
+	//metodos
+	
+	
+	
 }
--- Reto4/src/modelo/Personaje.java
+++ Reto4/src/modelo/Personaje.java
@@ -1,5 +1,76 @@
-package modelo;
 
 public class Personaje {
-
+	
+	//Valores
+	
+	private String nombre;
+	private String maestria;
+	private Habilidad [] habilidades;
+	private String categoria;
+	private int partidas;
+	private Usuario [] usuario;
+	
+	//getter setter
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getMaestria() {
+		return maestria;
+	}
+	public void setMaestria(String maestria) {
+		this.maestria = maestria;
+	}
+	public Habilidad[] getHabilidades() {
+		return habilidades;
+	}
+	public void setHabilidades(Habilidad[] habilidades) {
+		this.habilidades = habilidades;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public int getPartidas() {
+		return partidas;
+	}
+	public void setPartidas(int partidas) {
+		this.partidas = partidas;
+	}
+	public Usuario[] getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario[] usuario) {
+		this.usuario = usuario;
+	}
+	
+	//constructor
+	
+	public Personaje(String nombre, String maestria, Habilidad[] habilidades, String categoria, int partidas,
+			Usuario[] usuario) {
+		super();
+		this.nombre = nombre;
+		this.maestria = maestria;
+		this.habilidades = habilidades;
+		this.categoria = categoria;
+		this.partidas = partidas;
+		this.usuario = usuario;
+	}
+	
+	
+	//metodos
+	
+	public void subidaMaestria() {
+		//sube maestria de un personaje en un usuario concreto
+	}
+	
+	public void contadorPartidas() {
+		//cuenta todas as partidas jugadas con este personaje
+	}
+	
 }
