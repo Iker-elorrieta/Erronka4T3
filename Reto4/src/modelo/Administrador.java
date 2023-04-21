2a2550a7219731a2d7bf15878cb97cb2818c6b8d
in1dm3 <in1dm3@HZ352014.elorrieta.int>
2023-04-21 11:16:35 +0300

    Sprint 1 completo: Clases, Test y paquetes

--- Reto4/src/modelo/Administrador.java
+++ Reto4/src/modelo/Administrador.java
@@ -4,21 +4,9 @@ package modelo;
 
 public class Administrador extends Usuario {
 
-    
-
     public Administrador(int id, String nombre, String contrasena) {
 		super(id, nombre, contrasena);
-		// TODO Auto-generated constructor stub
 	}
-
-
-
-	@Override
-	public String toString() {
-		return "Administrador nombre" + nombre;
-	}
-
-
 
 	@Override
 	public boolean comprobarContrasenya(String contra) {
