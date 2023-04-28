bb4b3edff1ab01c79816280f0f5be324edc7a2cb
in1dm3 <in1dm3@HZ352014.elorrieta.int>
2023-04-28 11:12:54 +0300

    Sprint 2 6 tablas SQL

---
+++ Reto4/src/modelo/metodosTest.java
@@ -0,0 +1,14 @@
+package modelo;
+
+import org.junit.jupiter.api.Test;
+
+class metodosTest {
+
+	@Test
+	void testobtenerEstadisticas() {
+		String texto="";
+		Estadisticas est = new Estadisticas();
+		
+	}
+
+}
