147183491d1fa8a05668ef5b975c5aabb8987f4c
in1dm3 <in1dm3@HZ352014.elorrieta.int>
2023-05-08 13:59:14 +0300

    Casi test y selecciones complejas

--- Reto4/src/manager/GestionEstadisticas.java
+++ Reto4/src/manager/GestionEstadisticas.java
@@ -35,7 +35,7 @@ public class GestionEstadisticas {
 	        int kills = rand.nextInt(15) + 1; // Genera un número aleatorio entre 1 y 15
 	        int deaths = rand.nextInt(6); // Genera un número aleatorio entre 0 y 5
 	        int assists = rand.nextInt(17) + 4; // Genera un número aleatorio entre 4 y 20
-	        Estadisticas estadisticas= new Estadisticas(kills, deaths, assists);
+	        Estadisticas estadisticas= new Estadisticas(kills, deaths, assists) ;
 	        // Devuelve una cadena con las estadísticas aleatorias generadas
 	        return estadisticas;
 	    }
